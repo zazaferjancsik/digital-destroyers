57ca16615207633fbdc3f3137d3866ea0ec72c28
Annalisa Schipper <a.m.schipper1@students.uu.nl>
2024-01-22 15:52:04 +0100

    sandbox  split for gender

--- script_Annalisa.py
+++ script_Annalisa.py
@@ -10,4 +10,6 @@ for letter in filter_by_fic_character:
     for character in filter_by_fic_character[letter]:
         if "ontology/gender" in filter_by_fic_character[letter][character]:
             filtered_dictionaryCHARAC_GEN[character] =  filter_by_fic_character[letter][character]
+            filtered_dictionaryCHARAC_GEN[character][ontology/gender].split("/").reverse()[0]
 print()
+
