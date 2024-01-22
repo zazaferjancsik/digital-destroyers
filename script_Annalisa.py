c24cfd88205457ecabc639745eb618c4ad9801cf
Annalisa Schipper <a.m.schipper1@students.uu.nl>
2024-01-22 15:33:50 +0100

    code for dictionary for filtered data by fictional characters and presence of gender label

--- script_Annalisa.py
+++ script_Annalisa.py
@@ -3,6 +3,11 @@ import json
 with open() as filtered_json:
     filter_by_fic_character = filtered_json.read()
 
+
+filtered_dictionaryCHARAC_GEN= {}
+
 for letter in filter_by_fic_character:
     for character in filter_by_fic_character[letter]:
-        if filter_by_fic_character[letter][character][ontology/gender]
+        if "ontology/gender" in filter_by_fic_character[letter][character]:
+            filtered_dictionaryCHARAC_GEN[character] =  filter_by_fic_character[letter][character]
+print()
