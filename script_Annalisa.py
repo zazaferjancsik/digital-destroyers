06a2a3d84037bd7c94d89d874ec35dd822339949
Annalisa Schipper <a.m.schipper1@students.uu.nl>
2024-01-22 15:19:30 +0100

    sandox filter for gender

--- script_Annalisa.py
+++ script_Annalisa.py
@@ -1,5 +1,8 @@
-print("Hello Team")
-print("digital destroyers")
+import json
 
-print("Zaza checking in")
-print("Lucus")
+with open() as filtered_json:
+    filter_by_fic_character = filtered_json.read()
+
+for letter in filter_by_fic_character:
+    for character in filter_by_fic_character[letter]:
+        if filter_by_fic_character[letter][character][ontology/gender]
