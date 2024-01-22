69c79f317601e8378ffd41be8d228e4b37eba7a6
Annalisa Schipper <a.m.schipper1@students.uu.nl>
2024-01-22 16:44:35 +0100

    altered script?

--- script_Annalisa.py
+++ script_Annalisa.py
@@ -10,6 +10,12 @@ for letter in filter_by_fic_character:
     for character in filter_by_fic_character[letter]:
         if "ontology/gender" in filter_by_fic_character[letter][character]:
             filtered_dictionaryCHARAC_GEN[character] =  filter_by_fic_character[letter][character]
-            filtered_dictionaryCHARAC_GEN[character][ontology/gender].split("/").reverse()[0]
-print()
+            filtered_dictionaryCHARAC_GEN[character]["ontology/gender" ].split("/")
+            filtered_dictionaryCHARAC_GEN[character]["ontology/gender" ].reverse()
+            filtered_dictionaryCHARAC_GEN[character]["ontology/gender" ] =  filtered_dictionaryCHARAC_GEN[character]["ontology/gender" ][0]
+
+for character in filtered_dictionaryCHARAC_GEN:
+    print(filtered_dictionaryCHARAC_GEN[character]["ontology/gender"])
+
+
 
