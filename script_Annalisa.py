73665e2b0e1f5cd893788d167aa1974da6e82595
zaza_ferjancsik <zaza01@ferjancsik.hu>
2024-01-23 17:22:56 +0100

    Implemented augmented dataset to gender filtering

--- script_Annalisa.py
+++ script_Annalisa.py
@@ -6,7 +6,7 @@
 
 import json
 
-with open('filtered_characters.json', encoding='utf-8') as filtered_json:
+with open('filtered_characters_augmented.json', encoding='utf-8') as filtered_json:
     filter_by_fic_character = json.load(filtered_json)
 
 # create an empty dictionary to add all the filtered characters to by gender
@@ -26,6 +26,11 @@ for character_dictionary in filter_by_fic_character:
 # only want last entry so reverse (Male/Female appears last in gender ontology)
                     filtered_dictionaryCHARAC_GEN[name[character_number*2]]["ontology/gender" ].reverse()
                     filtered_dictionaryCHARAC_GEN[name[character_number*2]]["ontology/gender" ] =   filtered_dictionaryCHARAC_GEN[name[character_number*2]]["ontology/gender" ][0]
+                    filtered_dictionary_CHARAC_GEN_FOR_CSV.append({
+                        'fic_character': filtered_dictionaryCHARAC_GEN[name[character_number*2]]['title'],
+                        'gender' : filtered_dictionaryCHARAC_GEN[name[character_number*2]]['ontology/gender']
+                #'start_year': filtered_dictionaryCHARAC_GEN[character]['start_year']
+                        })
             elif type(character_dictionary[gender]) == list and gender == 'ontology/gender_label':
                 pass
             else:
@@ -37,33 +42,37 @@ for character_dictionary in filter_by_fic_character:
 # only want last entry so reverse (Male/Female appears last in gender ontology)
                 filtered_dictionaryCHARAC_GEN[character_dictionary['title']]["ontology/gender" ].reverse()
                 filtered_dictionaryCHARAC_GEN[character_dictionary['title']]["ontology/gender" ] =   filtered_dictionaryCHARAC_GEN[character_dictionary['title']]["ontology/gender" ][0]
-    
+                filtered_dictionary_CHARAC_GEN_FOR_CSV.append({
+                    'fic_character': filtered_dictionaryCHARAC_GEN[character_dictionary['title']]['title'],
+                    'gender' : filtered_dictionaryCHARAC_GEN[character_dictionary['title']]['ontology/gender']
+                    #'start_year': filtered_dictionaryCHARAC_GEN[character]['start_year']
+                    })
 
 # filtered_dictionary_CHARAC_GEN_FOR_CSV['gender'] = filtered_dictionaryCHARAC_GEN[letter['ontology/gender']]
 # only want last entry so reverse (Male/Female appears last in gender ontology)
        
 # not all characters have a start year for the series/movie --> use external dataset to obtain information
 # external dataset from Imdb for startyear of item (tsv file)
-count = 0
-with open('title.basics.tsv') as file:         
-    for line in file:
-        for character in filtered_dictionaryCHARAC_GEN:
-# if the name is the same for line in the imdb dataset to the name of item in DBpeople file, add the start year for the item to the dictionary made for the fictional characters
-            if 'ontology/firstAppearance' in filtered_dictionaryCHARAC_GEN[character]:
-                if line.split('\t')[2] == filtered_dictionaryCHARAC_GEN[character]['ontology/firstAppearance']:
-                    if line.split('\t')[5] != '\\N':
-                        filtered_dictionaryCHARAC_GEN[character]['start_year'] = int(line.split('\t')[5])
-                        filtered_dictionary_CHARAC_GEN_FOR_CSV.append({
-                                'fic_character': filtered_dictionaryCHARAC_GEN[character]['title'],
-                                'gender' : filtered_dictionaryCHARAC_GEN[character]['ontology/gender'],
-                                'start_year': filtered_dictionaryCHARAC_GEN[character]['start_year']
-                            })
-                        print("Done for "+str(filtered_dictionaryCHARAC_GEN[character]['title'])+ " line: "+ str(count))
-        count += 1
+# count = 0
+# with open('title.basics.tsv') as file:         
+#     for line in file:
+#         for character in filtered_dictionaryCHARAC_GEN:
+# # if the name is the same for line in the imdb dataset to the name of item in DBpeople file, add the start year for the item to the dictionary made for the fictional characters
+#             if 'ontology/firstAppearance' in filtered_dictionaryCHARAC_GEN[character]:
+#                 if line.split('\t')[2] == filtered_dictionaryCHARAC_GEN[character]['ontology/firstAppearance']:
+#                     if line.split('\t')[5] != '\\N':
+#                         filtered_dictionaryCHARAC_GEN[character]['start_year'] = int(line.split('\t')[5])
+#                         filtered_dictionary_CHARAC_GEN_FOR_CSV.append({
+#                                 'fic_character': filtered_dictionaryCHARAC_GEN[character]['title'],
+#                                 'gender' : filtered_dictionaryCHARAC_GEN[character]['ontology/gender'],
+#                                 'start_year': filtered_dictionaryCHARAC_GEN[character]['start_year']
+#                             })
+#                         print("Done for "+str(filtered_dictionaryCHARAC_GEN[character]['title'])+ " line: "+ str(count))
+#         count += 1
 import csv
 
 with open('dictionary_fictional_characters_gender.csv', 'w', newline='') as file:
-    fieldnames = ['fic_character', 'gender', 'start_year']
+    fieldnames = ['fic_character', 'gender']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
     for item in filtered_dictionary_CHARAC_GEN_FOR_CSV:
