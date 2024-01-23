95da12c807d1a32f19e7ea0bd13824c2ede53b37
Carla Ramakers <carlakerslo@gmail.com>
2024-01-23 14:55:16 +0100

    bar graph colored update

--- bar_fic_character.R
+++ bar_fic_character.R
@@ -14,18 +14,29 @@ filtered_gender <- character_list|>
                             gender == 'Male'~ 'Male',
                             TRUE~ NA)) |>
   na.omit() |>
-  filter(gender == 'Male'| gender== 'Female'| gender == 'Intersex')
+  filter(gender == 'Male'| gender== 'Female'| gender == 'Intersex') |>
+  unique() 
   
 
 ggplot(data = filtered_gender) + 
-  aes(x = gender, y = '')+
-  geom_col(position = 'stack')+
+  aes(x = gender)+
+  geom_bar()+
   labs(title = 'Gender Ratio of Fictional Characters') +
   xlab('Genders') +
   ylab('Number of Characters')+
-  theme_minimal()+
-  theme(panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank())
+  theme_minimal()
 
 ggsave('character_bar.pdf')
+
+ratio_gender <- ggplot(data = filtered_gender) +
+  aes(x= gender, fill = gender)+
+  geom_bar(aes(y = after_stat(count / sum(count)))) +
+  scale_y_continuous(labels = scales::percent)+
+  labs(title = 'Gender Ratio of Fictional Characters') +
+  xlab('Genders') +
+  ylab('Number of Characters')+
+  theme_minimal() +
+  scale_fill_manual(values = c('#b3c5f4','#ffc6c1','#b57edc'))
+  
+ggsave('ratio_gender.pdf')
 
