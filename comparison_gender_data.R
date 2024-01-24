8f59eaeef239b2923ff8a8e7619876f0bcba847a
Annalisa Schipper <a.m.schipper1@students.uu.nl>
2024-01-24 18:00:43 +0100

    comparison R changed + graph made

--- comparison_gender_data.R
+++ comparison_gender_data.R
@@ -14,10 +14,9 @@ filtered_gender <- character_list|>
                             gender == 'Man'~ 'Man',
                             gender == 'Male'~ 'Man',
                             TRUE~ NA)) |>
-  na.omit() |>
+  select(-start_year)|>
   filter(gender == 'Man'| gender== 'Woman') |>
-  unique() |>
-  mutate(method = 'inferred')
+  mutate(method = 'broadened sample')
 
 old_filtered_gender <- old_character_list|>
   mutate(gender = case_when(gender == 'Boy' ~ 'Man',
@@ -30,13 +29,36 @@ old_filtered_gender <- old_character_list|>
   na.omit() |>
   filter(gender == 'Man'| gender== 'Woman') |>
   unique() |>
-  mutate(method = 'original') 
+  mutate(method = 'sample') 
 
 together <- rbind(old_filtered_gender,filtered_gender)
 
-ggplot(data = together) +
-  aes(x= gender)+
-  geom_bar(aes(y = after_stat(prop), group = method, fill = gender)) +
+# ggplot(data =together) +
+#   aes(x= gender)+
+#   geom_bar(aes(y = after_stat(prop), group = method, fill = gender)) +
+#   scale_y_continuous(labels = scales::percent)+
+#   xlab('Genders') +
+#   ylab('Percentage of Characters')+
+#   facet_wrap(~method)+
+#   theme_minimal() +
+#   scale_fill_manual(values = c('#9dc190','#ffc594'))+
+#   theme(legend.position = "none")
+
+counts <- together |>
+  group_by(method, gender) |>
+  summarise(count = n())
+
+method_counts <- together |>
+  group_by(method) |>
+  summarise(group_count = n())
+
+counts <- counts |> 
+  left_join(method_counts) |>
+  mutate(percentage = count / group_count)
+
+ggplot(data = counts) +
+  aes(x = gender, y = percentage, fill = gender) +
+  geom_col()+
   scale_y_continuous(labels = scales::percent)+
   xlab('Genders') +
   ylab('Percentage of Characters')+
@@ -44,4 +66,3 @@ ggplot(data = together) +
   theme_minimal() +
   scale_fill_manual(values = c('#9dc190','#ffc594'))+
   theme(legend.position = "none")
-
