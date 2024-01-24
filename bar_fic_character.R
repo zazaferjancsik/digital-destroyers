358d0aae967824be2784f0b49e15992bbc2fef21
Carla Ramakers <carlakerslo@gmail.com>
2024-01-24 11:40:59 +0100

    changed labels to man woman and changed color

--- bar_fic_character.R
+++ bar_fic_character.R
@@ -6,37 +6,36 @@ library(stringr)
 character_list <- read_csv("dictionary_fictional_characters_gender.csv")
 
 filtered_gender <- character_list|>
-  mutate(gender = case_when(gender == 'Boy' ~ 'Male',
-                            gender == 'Female' ~'Female',
-                            gender == 'Hermaphrodite' ~ 'Intersex',
-                            gender == 'Girl'~ 'Female',
-                            gender == 'Man'~ 'Male',
-                            gender == 'Male'~ 'Male',
+  mutate(gender = case_when(gender == 'Boy' ~ 'Man',
+                            gender == 'Female' ~'Woman',
+                            gender == 'Girl'~ 'Woman',
+                            gender == 'Woman'~ 'Woman',
+                            gender == 'Man'~ 'Man',
+                            gender == 'Male'~ 'Man',
                             TRUE~ NA)) |>
   na.omit() |>
-  filter(gender == 'Male'| gender== 'Female'| gender == 'Intersex') |>
+  filter(gender == 'Man'| gender== 'Woman') |>
   unique() 
   
 
 ggplot(data = filtered_gender) + 
   aes(x = gender)+
   geom_bar()+
-  labs(title = 'Gender Ratio of Fictional Characters') +
   xlab('Genders') +
   ylab('Number of Characters')+
   theme_minimal()
 
 ggsave('character_bar.pdf')
 
-ratio_gender <- ggplot(data = filtered_gender) +
+ggplot(data = filtered_gender) +
   aes(x= gender, fill = gender)+
   geom_bar(aes(y = after_stat(count / sum(count)))) +
   scale_y_continuous(labels = scales::percent)+
-  labs(title = 'Gender Ratio of Fictional Characters') +
   xlab('Genders') +
-  ylab('Number of Characters')+
+  ylab('Percentage of Characters')+
   theme_minimal() +
-  scale_fill_manual(values = c('#b3c5f4','#ffc6c1','#b57edc'))
+  scale_fill_manual(values = c('#9dc190','#ffc594'))+
+  theme(legend.position = "none")
   
 ggsave('ratio_gender.pdf')
 
