2d1930be78d9e4722a3f1208fea3c5e1487ed269
Carla Ramakers <carlakerslo@gmail.com>
2024-01-25 16:16:15 +0100

    added comments to year_plotted R script

--- r_scripts/years_plotted.R
+++ r_scripts/years_plotted.R
@@ -6,6 +6,8 @@ library(lubridate)
 
 character_years <- read_csv("dictionary_fictional_characters_gender.csv")
 
+#replacing girl and female with woman, male and boy with man. filtering out 
+#other terms such as: cat.
 filtered_years <- character_years|>
   mutate(gender = case_when(gender == 'Boy' ~ 'Man',
                             gender == 'Female' ~'Woman',
@@ -15,28 +17,16 @@ filtered_years <- character_years|>
                             TRUE~ NA)) |>
   filter(gender == 'Man'| gender== 'Woman')
 
+#groping by gender and year to be able to count the amount of instances of 
+#each gender per year
 number <- filtered_years |>
   ungroup() |>
   group_by(gender,start_year) |>
   count(wt = NULL,sort = FALSE) |>
   filter(start_year>1900, start_year<2020)
 
-#percentage <- number |>
- # pivot_wider(names_from = gender,values_from = n ) |>
-  #arrange(start_year) |>
-  #mutate(
-   # Man = if_else(is.na(Man), 0, Man),
-    #Woman = if_else(is.na(Woman), 0, Woman))|>
-  #ungroup()|>
-#  na.omit()|>
- # group_by(start_year)|>
-#  mutate(total_sum = sum(Man+Woman))|>
- # mutate(Man_percent= Man/total_sum,
-  #       Woman_percent = Woman/total_sum)|>
-#  pivot_longer(cols = c('Man_percent','Woman_percent'),names_to = 'gender_per',values_to = 'percent')|>
- # pivot_longer(cols = c('Man','Woman'),names_to = 'gender',values_to = 'number')|>
-  #filter(start_year>1910)
-
+#filtering out years before 1919 and after 2019. using 'summar_bin' to 
+#add the counts per 25 years. Although it says 4 bins, the graph displays 5. 
 number |> filter(start_year >= 1919, start_year<=2019) |>
   ggplot()+ 
   aes(x=start_year, y=n,fill = gender) +
@@ -46,6 +36,10 @@ number |> filter(start_year >= 1919, start_year<=2019) |>
   ylab('Cummulative number of gender')+
   scale_fill_manual(values = c('#9dc190','#ffc594'))
 
+#moving the Man and Woman under gender to become their own columns. Replacing 
+# NA values with 0 to be able to later on do the cummulative sum. Arrange the 
+#years so that they go from oldest to most recent. After getting the cum. sum.
+# moving Man and Woman back to the column gender. 
 number_years <- number |>
   pivot_wider(names_from = gender,values_from = n ) |>
   mutate(
@@ -60,6 +54,7 @@ number_years <- number |>
   pivot_longer(cols = c('Man','Woman'),names_to = 'gender',values_to = 'number') |>
   filter(start_year>1900)
 
+#plotting the cummulative value of each gender throughout the years. 
 ggplot(data = number_years)+
   aes(x= start_year, y=number, color = gender)+
   geom_point()+
@@ -71,10 +66,12 @@ ggplot(data = number_years)+
   
 ggsave('Cummulative_number_gender_per_year.pdf')
 
+#calculating the ratio by dividing the cummulative Man by the Woman.
 ratio_data <- number_years |>
   mutate(ratio = Man / Woman)|>
   filter(start_year>1920)
 
+#creating scatterplot and connecting the points with a line.
 ggplot(data = ratio_data)+
   aes(x=start_year,y=ratio)+
   geom_point()+
