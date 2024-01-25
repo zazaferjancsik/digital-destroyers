f232aab77dc551916facb3d69a4261989c40901a
zaza_ferjancsik <zaza01@ferjancsik.hu>
2024-01-25 12:21:32 +0100

    debugged comparison script

--- py_scripts/script_comparison_probable.py
+++ py_scripts/script_comparison_probable.py
@@ -7,10 +7,10 @@ with open('probable_comparison.json', encoding='utf-8') as probable_json:
     counts = 0
     total_count = 0
     for character in filtered_and_probable:
-        if 'probable gender' in character:
+        if 'probable gender' in filtered_and_probable[character]:
             total_count += 1
-            if character["ontology/gender"] == character['probable gender']:
+            if filtered_and_probable[character]["ontology/gender"] == filtered_and_probable[character]['probable gender']:
                 counts += 1
-            if character['ontology/gender'] != character['probable gender']:
+            if filtered_and_probable[character]['ontology/gender'] != filtered_and_probable[character]['probable gender']:
                 faulty_counts += 1
     print(total_count, counts, faulty_counts)
