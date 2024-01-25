9de240169ec1abaec468aecf5137bcf21e59f65b
Carla Ramakers <carlakerslo@gmail.com>
2024-01-25 12:03:02 +0100

    Ratio and number scatterplot graph, and respective R script

--- years_plotted.R
+++ years_plotted.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(readr)
 library(ggplot2)
+library(dplyr)
+library()
 
 character_years <- read_csv("dictionary_fictional_characters_gender.csv")
 
@@ -11,9 +13,7 @@ filtered_years <- character_years|>
                             gender == 'Man'~ 'Man',
                             gender == 'Male'~ 'Man',
                             TRUE~ NA)) |>
-  na.omit() |>
-  filter(gender == 'Man'| gender== 'Woman') |>
-  unique() 
+  filter(gender == 'Man'| gender== 'Woman')
 
 number <- filtered_years |>
   ungroup() |>
@@ -33,14 +33,40 @@ ggplot(data = percentage)+
   ylab('Gender Percentage')+
   scale_color_manual(values = c('#9dc190','#ffc594'))
 
-ratio_data <- number |>
-  group_by(gender, start_year)|>
+number_years <- number |>
   pivot_wider(names_from = gender,values_from = n ) |>
+  mutate(
+    Man = if_else(is.na(Man), 0, Man),
+    Woman = if_else(is.na(Woman), 0, Woman)
+    )|>
   ungroup()|>
-  mutate(ratio = Man / Woman)
+  arrange(start_year)|>
+  na.omit()|>
+  mutate(Man=cumsum(Man),
+         Woman=cumsum(Woman))|>
+  pivot_longer(cols = c('Man','Woman'),names_to = 'gender',values_to = 'number') |>
+  filter(start_year>1900)
+
+ggplot(data = number_years)+
+  aes(x= start_year, y= number, color = gender)+
+  geom_point()+
+  guides(color = guide_legend('Gender'))+
+  xlab('Year')+
+  ylab('Cummulative Number')+
+  scale_color_manual(values = c('#9dc190','#ffc594'))+
+  theme_minimal()
+  
+ggsave('Cummulative_number_gender_per_year.pdf')
+
+ratio_data <- number_years |>
+  mutate(ratio = Man / Woman)|>
+  filter(start_year>1920)
 
 ggplot(data = ratio_data)+
   aes(x=start_year,y=ratio)+
-  geom_path()
+  geom_point()+
+  theme_minimal()+
+  xlab('Year')+
+  ylab('Ratio of Man to Woman')
 
-#geom_smooth(method=lm , color="red", se=FALSE) +
+ggsave('Ratio_Man_to_Woman.pdf')
